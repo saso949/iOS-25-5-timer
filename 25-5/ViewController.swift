233ac50032e38d0e524554147d7445bebddf45cb
saso949 <arukimakurou2@gmail.com>
2021-03-22 12:12:27 +0900

    first commit

---
+++ 25-5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  25-5
+//
+//  Created by Aoba S on 2021/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
